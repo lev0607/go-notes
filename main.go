8f1532d4dcec6c03bfee45d804f3193414d17893
lev0607 <lev36rus@gmail.com>
2022-02-15 22:23:49 +0300

    add handlers

--- main.go
+++ main.go
@@ -6,13 +6,33 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("It`s work"))
+}
+
+func showNote(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Отображение заметки..."))
+}
+
+func createNote(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Метод запрещен!", 405)
+		return
+	}
+	w.Write([]byte("Форма для создания новой заметки..."))
 }
 
 func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/note", showNote)
+	mux.HandleFunc("/note/create", createNote)
 
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
