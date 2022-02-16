316339dbbd7a2a236dae6469bf83859ed3c2c729
lev0607 <lev36rus@gmail.com>
2022-02-16 21:29:37 +0300

    refactor

---
+++ cmd/web/handlers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("It`s work"))
+}
+
+func showNote(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
+}
+
+func createNote(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Метод запрещен!", 405)
+		return
+	}
+	w.Write([]byte("Форма для создания новой заметки..."))
+}
